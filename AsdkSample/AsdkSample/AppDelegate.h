f4031d21b9b58e1b94ecbd356d7062d557f48e91
cBeamChang <bemchang@me.com>
2017-12-24 15:29:58 +0800

    AsdkSample
    初始化当前工程文件

---
+++ AsdkSample/AsdkSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsdkSample
+//
+//  Created by ch_int_beam on 2017/12/24.
+//  Copyright © 2017年 ch_int_beam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AsdkSample/AsdkSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AsdkSample
+//
+//  Created by ch_int_beam on 2017/12/24.
+//  Copyright © 2017年 ch_int_beam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
